35eaf1db1928c1eea9aecab156adba66124c2b5a
Evan Sebastian <evanlhoini@gmail.com>
2016-08-24 09:59:01 +0800

    Disable webpack dashboard

--- scripts/serve.js
+++ scripts/serve.js
@@ -5,21 +5,15 @@ const WebpackDevServer = require('webpack-dev-server')
 const webpack = require('webpack')
 const createWebpackConfig = require('../webpack')
 
-const Dashboard = require('webpack-dashboard')
-const DashboardPlugin = require('webpack-dashboard/plugin')
-const dashboard = new Dashboard()
-
 module.exports = (bundleDir, template) => {
   process.env.NODE_ENV = 'development'
   const webpackConfig = createWebpackConfig(bundleDir, template)
-  webpackConfig.plugins.push(new DashboardPlugin(dashboard.setData))
   const compiler = webpack(webpackConfig)
   new WebpackDevServer(compiler, {
     hot: true,
     publicPath: webpackConfig.output.publicPath,
     historyApiFallback: true,
     noInfo: false,
-    quiet: true,
     stats: {
       chunkModules: false,
       colors: true
