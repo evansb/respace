2306d76740bb5e4fa84e47dbc512590c11420bf5
Evan Sebastian <evanlhoini@gmail.com>
2017-03-16 03:05:37 +0800

    Use Blueprint components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,27 @@
+import { Button, Intent } from '@blueprintjs/core'
 import * as React from 'react'
 
 export function Navbar() {
+
+  const toggleSidebarButton =
+    <Button className='pt-minimal pt-active' iconName='one-column' />
+
+  const submitButton =
+    <Button iconName='upload'
+            intent={Intent.PRIMARY}>
+      Submit
+    </Button>
+
   return (
     <nav className='pt-navbar pt-fixed-top'>
       <div className="pt-navbar-group pt-align-left">
-        <button className='pt-button pt-minimal pt-active pt-icon-one-column'>
-        </button>
+        {toggleSidebarButton}
         <div className="pt-navbar-heading">
            Respace
         </div>
       </div>
       <div className="pt-navbar-group pt-align-right">
-          <button className='pt-button pt-intent-primary pt-icon-upload'>
-            Submit
-          </button>
+        {submitButton}
       </div>
     </nav>
   )
