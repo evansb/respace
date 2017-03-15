98f294cb9d4d9417c7b7733a5f1d6f1f61b21458
Evan Sebastian <evanlhoini@gmail.com>
2017-03-16 02:58:11 +0800

    Add Submit Button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,16 @@ export function Navbar() {
   return (
     <nav className='pt-navbar pt-fixed-top'>
       <div className="pt-navbar-group pt-align-left">
-        <div className="pt-navbar-heading">Respace</div>
+        <button className='pt-button pt-minimal pt-active pt-icon-one-column'>
+        </button>
+        <div className="pt-navbar-heading">
+           Respace
+        </div>
       </div>
       <div className="pt-navbar-group pt-align-right">
+          <button className='pt-button pt-intent-primary pt-icon-upload'>
+            Submit
+          </button>
       </div>
     </nav>
   )
