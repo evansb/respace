5bb47623bd435dc924467bc674e940759abd9a57
Evan Sebastian <evanlhoini@gmail.com>
2017-03-21 16:49:34 +0800

    Bundle to Surge

--- build/webpack.config.production.ts
+++ build/webpack.config.production.ts
@@ -1,6 +1,7 @@
 /// <reference path='webpack.fix.d.ts' />
 
 import * as ExtractTextPlugin from 'extract-text-webpack-plugin'
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'
 import * as path from 'path'
 import * as webpack from 'webpack'
 import * as ParallelUglifyPlugin from 'webpack-parallel-uglify-plugin'
@@ -17,7 +18,10 @@ export default merge(baseConfig, {
   devtool: 'cheap-module-source-map',
 
   entry: {
-    app: ['babel-polyfill', path.resolve(__dirname, '../examples', entryPoint, 'index.ts')],
+    app: [
+      'babel-polyfill',
+       path.resolve(__dirname, '../examples', entryPoint, 'index.ts')
+    ],
     blueprintjs: [ './src/blueprintjs' ]
   },
 
@@ -27,7 +31,7 @@ export default merge(baseConfig, {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
+          use: ['css-loader?minimize', 'postcss-loader', 'sass-loader']
         })
       }
      ]
@@ -44,8 +48,16 @@ export default merge(baseConfig, {
     new ParallelUglifyPlugin({
       cacheDir: path.resolve(__dirname, '../.uglifycache')
     }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: 'body',
+      template: path.resolve(__dirname, '../index.html')
+    }),
     new webpack.optimize.OccurrenceOrderPlugin(false),
-    new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
+    new ExtractTextPlugin({
+      allChunks: true,
+      filename: '[name].min.css'
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production')
